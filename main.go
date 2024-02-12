1dd36f8bb63a02a9b73f0959bd67d839c9f81b41
Paz <50664967+TwoWaySix@users.noreply.github.com>
2024-02-12 17:14:16 +0100

    initial

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	_ "embed"
+)
+
+//go:embed static/index.html
+var index []byte
+
+func main() {
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/download/", http.StripPrefix("/download/", fs))
+	// http.HandleFunc("/", serveIndex)
+	log.Fatal(http.ListenAndServe(":8910", nil))
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write(index)
+}
