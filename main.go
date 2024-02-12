5d0918dc692b9cc55435a0134421deafc9745404
Paz <50664967+TwoWaySix@users.noreply.github.com>
2024-02-12 23:18:45 +0100

    big cleanup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,6 +16,8 @@ type Entry struct {
 	Directory string
 	IsDir     bool
 }
+
+var port = 8910
 
 //go:embed static/index.html
 var index []byte
@@ -30,7 +33,6 @@ func init() {
 		log.Fatal(err)
 	}
 	templ = t
-
 }
 
 func getDirEntries(directory string) ([]Entry, error) {
@@ -53,10 +55,12 @@ func getDirEntries(directory string) ([]Entry, error) {
 func main() {
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/api/download/", http.StripPrefix("/api/download/", fs))
+
 	http.HandleFunc("/", serveIndex)
-	// http.HandleFunc("/api/data", serveData)
 	http.HandleFunc("/api/list/{path...}", renderEntries)
-	log.Fatal(http.ListenAndServe(":8910", nil))
+
+	fmt.Printf("Open a browser on http://localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
 
 func renderEntries(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +69,6 @@ func renderEntries(w http.ResponseWriter, r *http.Request) {
 		subPath = "."
 	}
 
-	log.Printf("subPath: '%s'", subPath)
 	entries, err := getDirEntries(subPath)
 	if err != nil {
 		log.Println(err)
@@ -83,28 +86,3 @@ func renderEntries(w http.ResponseWriter, r *http.Request) {
 func serveIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write(index)
 }
-
-// func OLDlistContent(w http.ResponseWriter, r *http.Request) {
-// 	subPath := r.PathValue("path")
-// 	entries, err := os.ReadDir(path.Join(directory, subPath))
-// 	if err != nil {
-// 		log.Println(err)
-// 		w.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	files := make([]Entry, 0)
-// 	for _, entry := range entries {
-// 		files = append(files, Entry{
-// 			Name:  entry.Name(),
-// 			IsDir: entry.IsDir(),
-// 		})
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	err = json.NewEncoder(w).Encode(files)
-// 	if err != nil {
-// 		log.Println(err)
-// 		return
-// 	}
-// }
